1bf5eb7cabcea4cf115e22cbf840e62c8ca4d13f
Daniel <danieloliveiradesouza09@outlook.com>
2024-06-14 16:44:26 -0300

    initial commit

---
+++ src/main/Contador.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.util.Scanner;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro:");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			System.out.println(exception.getMessage());
+		}finally {
+			terminal.close();
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois)
+			throw new ParametrosInvalidosException();
+		
+		int contagem = parametroDois - parametroUm;
+		for (int i = 1; i <= contagem; i++) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ src/main/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package main;
+
+public class ParametrosInvalidosException extends Exception {
+	public ParametrosInvalidosException () {
+		super("O segundo parâmetro deve ser maior que o primeiro");
+	}
+}
